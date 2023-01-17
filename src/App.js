7ed8c89b7eec5e97e718340f30b88d7c51df2d84
Ben <bjporter123@gmail.com>
2023-01-17 09:32:51 +0000

    week_07_day_01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [todos, settodos] = useState([
+    {id : 1, name : "Walk Dogs"},
+    {id: 2, name : "Clean Car"},
+    {id: 3, name : "Do Shopping"}
+  ])
+  const [newItem, setNewItem] = useState("")
+
+  const todoListtodos = todos.map((item)=>{
+    return(
+    <li key={item.id}>
+      {item.name}<button onClick = {() => removeItemOnList(item.id)}>Remove To Do List Item</button>
+    </li>
+    
+  )
+  })
+
+  const removeItemOnList = (id) => {
+    const newToDoList = todos.filter((item) => item.id !== id)
+    settodos(newToDoList)
+  }
+  
+    const handleItemInput = (evt) => {
+    setNewItem(evt.target.value)
+  }
+
+  const saveNewItem = (evt) => {
+    evt.preventDefault()
+    const newItemObject = {id : Date.now(), name : newItem}
+    const nextItemList = [...todos, newItemObject]
+    settodos(nextItemList)
+    setNewItem("")
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="title">Todo List</h1>
+    <hr></hr>
+    <form className= "todo-form"onSubmit={saveNewItem}>
+      <label htmlFor= "new-item">New ToDo:</label>
+      <input required id= "new-item" type = "text" value = {newItem}  onChange={handleItemInput}></input>
+      <input  type="submit" value="Save Item"></input>
+    </form>
+
+    <ul>{todoListtodos}</ul>
+
+    <form></form>
+  </div>
   );
 }
 
 export default App;
+
